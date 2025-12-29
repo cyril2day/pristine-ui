d07268a7798692446a3b825746a06d7979c4f2e0
psi <cy.lonsido@gmail.com>
2025-12-29 20:36:50 +0800

    update typography, revamp styles

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <button>Sample Button</button>
 </template>
 
 <style scoped></style>
